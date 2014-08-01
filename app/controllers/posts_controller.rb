69b2f978c1b7149e3f3d7c3927b7cf471d4007bf
Daneil Wei <daneilmu1@gmail.com>
2014-08-01 18:19:18 +0800

    add create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.create(post_params)
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content, :created_at, :updated_at)
+    end
 end
